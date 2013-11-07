a3a9c5f270df9f0ac1436ce3953c3f00984943a1
nickabbey <nickabey@gmail.com>
2013-11-07 03:19:30 -0500

    iProject 3 complete. GladOs disabled TODO - Clean up code and update comments (Merge main in to gh-pages AND scheduling branches after)

--- scripts/host/control.js
+++ scripts/host/control.js
@@ -75,14 +75,14 @@ function hostInit()
    // Check for our testing and enrichment core.
 //   if (typeof _GLaDOS === "function")
 //   {
-       //Compiler will see this as an unresolved type if alan's test script isn't enabled in index.html
-      _GLaDOS = new Glados();
-       alert("ALERT! - Changes to console IO don't play nice with GlaDOS. " +
-           "The script executes, but this disables keyboard input! " +
-            "Luckily, you can purge the Neurotoxins after the script has run. " +
-            "Just click the 'Purge' button when you want to regain control");
-      _GLaDOS.init();
-      _Testing = true;
+       //Interpreter will see this as an unresolved type if alan's test script isn't enabled in index.html
+//      _GLaDOS = new Glados();
+//       alert("ALERT! - Changes to console IO don't play nice with GlaDOS. " +
+//           "The script executes, but this disables keyboard input! " +
+//            "Luckily, you can purge the Neurotoxins after the script has run. " +
+//            "Just click the 'Purge' button when you want to regain control");
+//      _GLaDOS.init();
+//      _Testing = true;
 //   }
 
 //    // Personal testing
